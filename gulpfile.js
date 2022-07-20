b403f954219c97efcc3e8767d21c93aa7d9e6de9
dima <dima.xipkov@gmail.com>
2022-07-20 15:37:52 +0300

    last1

--- gulpfile.js
+++ gulpfile.js
@@ -20,35 +20,34 @@ const svgSprite = require('gulp-svg-sprite');
 // gulp-svg-sprite
 // const { src, dest, parallel } = require('gulp');
 
-const svgspriteConfig = {
-    mode: {
-        // css: {
-        //     render: {
-        //         css: true
 
-        //     }
-        // }
-        stack: {
-            sprite: '../icons.svg',
-            example: true
-        }
-    }
 
-};
-
-function icondel() {
-    return del('app/images/icons/**/*.svg')
-}
+// function icondel() {
+//     return del('app/images/icons/**/*.svg')
+// }
 
 function buildSvg() {
     return src('app/images/icons/**/*.svg')
-        .pipe(svgSprite(svgspriteConfig))
+        .pipe(svgSprite({
+            mode: {
+            // css: {
+            //     render: {
+            //         css: true
+
+            //     }
+            // }
+            stack: {
+                sprite: '../icons.svg',
+                example: true
+            }}
+        }))
         .pipe(dest('app/images/sprite'));
-    // .pipe(cleanDist('app/images/icons/**/*.svg'));
-
-
-
 }
+
+// const svgspriteConfig = {
+
+
+// };
 // app/images/
 // .pipe(dest('tmp/gulp'));
 // exports.default = parallel(buildSvg);
@@ -184,13 +183,13 @@ function watching() {
 // ----------------------------------------------------
 // вызов функции отдельно
 const { task } = require('gulp');
-task(gule);
+// task(gule);
 
-function gule() {
-    return src('app/images/icons/**/*.svg')
-        .pipe(svgSprite(svgspriteConfig))
-        .pipe(dest('app/images/sprite'));
-}
+// function gule() {
+//     return src('app/images/icons/**/*.svg')
+//         .pipe(svgSprite(svgspriteConfig))
+//         .pipe(dest('app/images/sprite'));
+// }
 // ----------------------------------------------------
 
 const fonter = require('gulp-fonter');
@@ -234,21 +233,19 @@ function fonts() {
 
 const ttf2woff2 = require('gulp-ttf2woff2');
 
-task(convert);
+// task(convert);
 
-function convert() {
-    // gulp.src(['fonts/*.ttf'])
+function ttfToWoff() {
     return src('app/fonts/*')
         .pipe(ttf2woff())
-        // .pipe(ttf2woff2())
         .pipe(dest('app/fonts'))
-    // .pipe('app/fonts/*')
-    // .pipe(ttf2woff2())
-    // .pipe(dest('app/fonts'));
+        .pipe(src(`${fontsFile2}`))
+        .pipe(ttf2woff2())
+        .pipe(dest('app/fonts'));
 }
 // ----------------------------------------------------
 const ttf2woff = require('gulp-ttf2woff');
-
+const fontsFile2 = 'app/fonts/*'
 task(convert2);
 
 function convert2() {
@@ -265,63 +262,7 @@ function convert2() {
 //     .pipe(ttf2woff2());
 // }
 
-// ----------------------------------------------------
-// 
-//проверяем существуют ли файлы шрифтов
-//     fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
-//         if (fontsFiles) {
-//             //проверяем существует ли файл стилей для подключения шрифтов
-//             if (!fs.existsSync(fontsFile)) {
-//                 /*ТУТ*/
-//                 //если файла нет создаем его
-//                 fs.writeFile(fontsFile, '', cb);
-//                 let newFileOnly;
-//                 for (var i = 0; i < fontsFiles.length; i++) {
-//                     //записываем подключения шрифтов в файл стилей 
-//                     let fontFileName = fontsFiles[i].split('.')[0];
-//                     if (newFileOnly !== fontFileName) {
-//                         let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
-//                         let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
-//                         if (fontWeight.toLowerCase() === 'thin') {
-//                             fontWeight = 100;
-//                         } else if (fontWeight.toLowerCase() === 'extralight') {
-//                             fontWeight = 200;
-//                         } else if (fontWeight.toLowerCase() === 'light') {
-//                             fontWeight = 300;
-//                         } else if (fontWeight.toLowerCase() === 'medium') {
-//                             fontWeight = 500;
-//                         } else if (fontWeight.toLowerCase() === 'semibold') {
-//                             fontWeight = 600;
-//                         } else if (fontWeight.toLowerCase() === 'bold') {
-//                             fontWeight = 700;
-//                         } else if (fontWeight.toLowerCase() === 'extrabold') {
-//                             fontWeight = 800;
-//                         } else if (fontWeight.toLowerCase() === 'black') {
-//                             fontWeight = 900;
-//                         } else {
-//                             fontWeight = 400;
-//                         }
-//                         fs.appendFile(fontsFile, `@font-face {\n\tfont-famaly: ${fontName};\n\tfont-display:swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");font-weight:${fontWeight};\n\tfont-style:normal;\n;}\r\n`, cb);
-//                         newFileOnly = fontFileName;
-//                     }
-//                 }
-//             } else {
-//                 //если есть файл,выводим сообщение
-//                 console.log("файл scss/fonts.scss уже существует. Для обновления нужноно его удалить")
-//             }
-//         }
-//     });
-// }
-// ----------------------------------------------------
 
-// task(reed);
-
-// function reed() {
-//     fs.ensureFile(file, err => {
-//         console.log(err) // => null
-//         // file has now been created, including the directory it is to be placed in
-//     })
-// }
 
 const fs = require('fs-extra')
 
@@ -338,139 +279,57 @@ const fs = require('fs-extra')
 //     })
 // }
 
-task(convert4);
-function convert4() {
-    // if (file) {
-    //     console.log("файл есть")
-    // } else {
-    //     //если есть файл,выводим сообщение
-    //     console.log("файл scss/fonts.scss уже существует. Для обновления нужноно его удалить")
-    // }
-    // fontsFiles = file
-    // let fontsFile = `app/_fonts.scss`;
-    // let fontsFile = `${.srcFolder}app/_fonts.scss`;
-    // получаемв переменыупуть к файлу
-
-    fs.access("app/_fonts.scss", function (error) {
-        // if (error) {
-        //     console.log("Файл не найден");
-        //     fs.ensureFile(file, err => {
-        //         console.log(err) // => null
-        //         // file has now been created, including the directory it is to be placed in
-        //     })
-        // } else {
-        //     console.log("Файл найден");
-        if (!fs.existsSync(fontsFile)) {
-            fs.writeFile(fontsFile, '', cb);
-            // верх создант файл
-            let newFileOnly;
-            for (var i = 0; i < fontsFiles.length; i++) {
-                let fontFileName = fontsFiles[i].split('.')[0];
-                if (newFileOnly !== fontFileName) {
-                    let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
-                    let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
-                    if (fontWeight.toLowerCase() === 'thin') {
-                        fontWeight = 100;
-                    } else if (fontWeight.toLowerCase() === 'extralight') {
-                        fontWeight = 200;
-                    } else if (fontWeight.toLowerCase() === 'light') {
-                        fontWeight = 300;
-                    } else if (fontWeight.toLowerCase() === 'medium') {
-                        fontWeight = 500;
-                    } else if (fontWeight.toLowerCase() === 'semibold') {
-                        fontWeight = 600;
-                    } else if (fontWeight.toLowerCase() === 'bold') {
-                        fontWeight = 700;
-                    } else if (fontWeight.toLowerCase() === 'extrabold') {
-                        fontWeight = 800;
-                    } else if (fontWeight.toLowerCase() === 'black') {
-                        fontWeight = 900;
-                    } else {
-                        fontWeight = 400;
-                    }
-                    fs.appendFile(fontsFile, `@font-face {\n\tfont-famaly: ${fontName};\n\tfont-display:swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");font-weight:${fontWeight};\n\tfont-style:normal;\n;}\r\n`, cb);
-                    newFileOnly = fontFileName;
-                    console.log("работает");
-                }
-            }
-        }
-        // }
-    });
-    // pipe(dest(`${srcFolder}/fonts/`))
-    // return src(`app/_fonts.scss`);
-    // return src(`${srcFolder}`);
-
-    function cb() { }
-}
-// ./src'
-const fontsFile = 'app/_fonts.scss'
+const fontsFile = 'app/scss/_fonts.scss'
 const srcFolder = 'app/fonts';
-const length = 1;
+
 // папка с исходниками
 
-task(convert5);
-function convert5() {
-
-    // let fontsFile = `${srcFolder}app/_fonts.scss`
-    // let fontsFile = `${app.path.srcFolder}/sscss/fonts.scss`;
-    console.log("1")
+task(fontsStyle);
+function fontsStyle() {
     fs.readdir(srcFolder, function (err, fontsFiles) {
-        // !!!возможно ошибка
-        console.log("1.2")
-        // if (fontsFiles) {
-        console.log("2")
-        //проверяем существует ли файл стилей для подключения шрифтов
-        if (!fs.existsSync(fontsFile)) {
-            console.log("3")
-            /*ТУТ*/
-            //если файла нет создаем его
-            fs.writeFile(fontsFile, '', cb);
-            console.log("4")
-            let newFileOnly;
-
-            // !!!потом удалить лет переменныю
-            for (var i = 0; i < fontsFiles.length; i++) {
-                console.log("5")
-                //записываем подключения шрифтов в файл стилей 
-                let fontFileName = fontsFiles[i].split('.')[0];
-                if (newFileOnly !== fontFileName) {
-                    console.log("6")
-                    let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
-                    let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
-                    if (fontWeight.toLowerCase() === 'thin') {
-                        fontWeight = 100;
-                    } else if (fontWeight.toLowerCase() === 'extralight') {
-                        fontWeight = 200;
-                    } else if (fontWeight.toLowerCase() === 'light') {
-                        fontWeight = 300;
-                    } else if (fontWeight.toLowerCase() === 'medium') {
-                        fontWeight = 500;
-                    } else if (fontWeight.toLowerCase() === 'semibold') {
-                        fontWeight = 600;
-                    } else if (fontWeight.toLowerCase() === 'bold') {
-                        fontWeight = 700;
-                    } else if (fontWeight.toLowerCase() === 'extrabold') {
-                        fontWeight = 800;
-                    } else if (fontWeight.toLowerCase() === 'black') {
-                        fontWeight = 900;
-                    } else {
-                        fontWeight = 400;
+        if (fontsFiles) {
+            //проверяем существует ли файл стилей для подключения шрифтов
+            if (!fs.existsSync(fontsFile)) {
+                //если файла нет создаем его
+                fs.writeFile(fontsFile, '', cb);
+                let newFileOnly;
+                for (var i = 0; i < fontsFiles.length; i++) {
+                    //записываем подключения шрифтов в файл стилей 
+                    let fontFileName = fontsFiles[i].split('.')[0];
+                    if (newFileOnly !== fontFileName) {
+                        let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
+                        let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
+                        if (fontWeight.toLowerCase() === 'thin') {
+                            fontWeight = 100;
+                        } else if (fontWeight.toLowerCase() === 'extralight') {
+                            fontWeight = 200;
+                        } else if (fontWeight.toLowerCase() === 'light') {
+                            fontWeight = 300;
+                        } else if (fontWeight.toLowerCase() === 'medium') {
+                            fontWeight = 500;
+                        } else if (fontWeight.toLowerCase() === 'semibold') {
+                            fontWeight = 600;
+                        } else if (fontWeight.toLowerCase() === 'bold') {
+                            fontWeight = 700;
+                        } else if (fontWeight.toLowerCase() === 'extrabold') {
+                            fontWeight = 800;
+                        } else if (fontWeight.toLowerCase() === 'black') {
+                            fontWeight = 900;
+                        } else {
+                            fontWeight = 400;
+                        }
+                        fs.appendFile(fontsFile, `@font-face {\n\tfont-family: '${fontName}';\n\tfont-weight:${fontWeight};\n\tfont-style:normal;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-display:swap;\n}\r\n`, cb);
+                        newFileOnly = fontFileName;
                     }
-                    fs.appendFile(fontsFile, `@font-face {\n\tfont-family: '${fontName}';\n\tfont-weight:${fontWeight};\n\tfont-style:normal;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-display:swap;\n}\r\n`, cb);
-                    newFileOnly = fontFileName;
-                    
                 }
+            } else {
+                //если есть файл,выводим сообщение
+                console.log("файл scss/fonts.scss уже существует. Для обновления нужноно его удалить")
             }
-        } else {
-            //если есть файл,выводим сообщение
-            console.log("файл scss/fonts.scss уже существует. Для обновления нужноно его удалить")
         }
-        // }
     });
     return src(`${srcFolder}`);
-    // return app.gulp.src(`${app.path.srcFolder}`);
-    // возможно ошибка
-    // return app.gulp.src(`${app.path.srcFolder}`);
+
     function cb() { }
 }
 // 
@@ -484,7 +343,7 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build, buildSvg);
 //
-exports.default = parallel(styles, scripts, browsersync, watching, buildSvg, convert, convert2, convert5);
+exports.default = parallel(styles, scripts, browsersync, watching, buildSvg, ttfToWoff, fontsStyle);
 
 // const fs = require('fs');
 
